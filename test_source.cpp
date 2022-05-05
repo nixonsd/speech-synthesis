c372d6aa8ac2e4521ced81f5c74d9e1233b32f09
Bogdan <bandanovvs@gmail.com>
2022-05-05 13:56:42 +0300

    Update .gitignore

---
+++ test_source.cpp
@@ -0,0 +1,6 @@
+#include "test_source.h"
+
+test_source::test_source()
+{
+
+}
---
+++ test_source.h
@@ -0,0 +1,11 @@
+#ifndef TEST_SOURCE_H
+#define TEST_SOURCE_H
+
+
+class test_source
+{
+public:
+    test_source();
+};
+
+#endif // TEST_SOURCE_H
